0fe3de13adbd7dd07d9079e1359de6aae4afe614
kriegore <erik.c.krieg+kriegore@gmail.com>
2020-05-14 19:27:28 -0400

    test taretunit hook

--- index.lua
+++ index.lua
@@ -6,9 +6,9 @@ end
 
 Paix:test()
 
-hooksecurefunc("Unit_UnitName", function (self, name)
+hooksecurefunc(TargetUnit, function (unit)
   message("unit name called")
-  print(name)
+  print(unit)
 end)
 
 -- POC part 1
