0000d106ab8c75230afdb15a5e5b0c6669fa7999
kriegore <erik.c.krieg+kriegore@gmail.com>
2020-05-18 14:06:45 -0400

    adds function for enabling nice event registering interface for frames in lib

---
+++ lib/frame.lua
@@ -0,0 +1,20 @@
+-- Provides a Frame with a terse syntax for 
+-- adding event listeners.
+--
+-- Psuedo code example:
+--  frame = enableEvents(CreateFrame("Frame"))
+--  frame.onEvent(EXAMPLE_EVENT, function (self, event) ... end)
+--
+local function enableEvents(f)
+  f._events = {}
+  
+  f:SetScript("OnEvent", function(self, event, ...)
+    return self._events[event](self, event, ...)
+  end)
+
+  function f.onEvent(event, handler)
+    f._events[event] = handler
+  end
+
+  return f
+end
